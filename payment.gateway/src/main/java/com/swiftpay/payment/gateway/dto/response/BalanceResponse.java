cbcdac8a7673fce3b906edf4f7a6960b1e0facd1
Gottipati Gopi Sai <gottipatigopisai@gmail.com>
2026-05-24 13:07:13 +0530

    add swagger documentation for apis

--- payment.gateway/src/main/java/com/swiftpay/payment/gateway/dto/response/BalanceResponse.java
+++ payment.gateway/src/main/java/com/swiftpay/payment/gateway/dto/response/BalanceResponse.java
@@ -1,10 +1,17 @@
 package com.swiftpay.payment.gateway.dto.response;
 
 import java.math.BigDecimal;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
+@Schema(description = "Response details containing the account balance")
 public class BalanceResponse {
+
+    @Schema(
+        description = "The current available balance of the account", 
+        example = "1250.45", 
+        requiredMode = Schema.RequiredMode.REQUIRED
+    )
     private BigDecimal balance;   
 }
